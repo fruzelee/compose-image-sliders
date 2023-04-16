e51faaf4b0d32dc95d759674e79752964fddba73
fruzelee <fazle.rabbi.cse@gmail.com>
2023-04-16 16:20:49 +0600

    feat(slider): update sliders graphicsLayer

--- app/src/main/java/com/example/imagesliders/CarouselCard.kt
+++ app/src/main/java/com/example/imagesliders/CarouselCard.kt
@@ -1,46 +1,69 @@
 package com.example.imagesliders
 
-import androidx.compose.foundation.ExperimentalFoundationApi
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.size.Scale
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import com.google.accompanist.pager.rememberPagerState
+import kotlin.math.absoluteValue
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun CarouselCard() {
     val pagerState = rememberPagerState(initialPage = 2)
     val sliderList = listOf(
+        "https://picsum.photos/id/239/500/800",
         "https://picsum.photos/id/240/500/800",
         "https://picsum.photos/id/241/500/800",
         "https://picsum.photos/id/242/500/800",
         "https://picsum.photos/id/243/500/800",
-        "https://picsum.photos/id/244/500/800",
-        "https://picsum.photos/id/245/500/800"
+        "https://picsum.photos/id/244/500/800"
     )
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
         HorizontalPager(
-            pageCount = sliderList.size,
+            count = sliderList.size,
             state = pagerState,
             contentPadding = PaddingValues(horizontal = 65.dp),
             modifier = Modifier
                 .height(350.dp)
         ) { page ->
             Card(
-                shape = RoundedCornerShape(10.dp)
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .graphicsLayer {
+                        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+                        lerp(
+                            start = 0.50f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                            .also { scale ->
+                                scaleX = scale
+                                scaleY = scale
+                            }
+                    }
             ) {
                 AsyncImage(
                     model = ImageRequest.Builder(LocalContext.current)
@@ -56,6 +79,8 @@ fun CarouselCard() {
             }
 
         }
+
+
 
     }
 }
